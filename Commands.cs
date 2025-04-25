4b0b0b08b99fd07916d0f45648bfdd4348554706
phlebotomist <phlebotomist>
2025-04-25 12:38:57 -0700

    put space back in for debug placeholder value *

--- Commands.cs
+++ Commands.cs
@@ -12,16 +12,16 @@ public class Commands
 {
 	public static string Pad(string s, int n)
 	{
-		return new string('*', n) + s + new string('*', n);
+		return new string(' ', n) + s + new string(' ', n);
 	}
 
 	public static string PadR(string s, int n)
 	{
-		return s + new string('*', n);
+		return s + new string(' ', n);
 	}
 	public static string PadL(string s, int n)
 	{
-		return new string('*', n) + s;
+		return new string(' ', n) + s;
 	}
 
 	[Command("leaderboard", shortHand: "kf top")]
